ccaf7529ff4e42455bec734b61b87029f70350c2
George Hotz <geohot@gmail.com>
2023-08-23 09:23:12 -0700

    lazy makes it fast

--- import_from_tinygrad.py
+++ import_from_tinygrad.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import pathlib
 
-FILES = ["tensor.py", "mlops.py", "nn/optim.py"]
+FILES = ["tensor.py", "mlops.py", "helpers.py", "nn/optim.py", "ops.py", "runtime/lib.py", "runtime/ops_cpu.py", "shape/shapetracker.py", "shape/symbolic.py"]
+#FILES += ["lazy.py"]
 src = pathlib.Path("../tinygrad/tinygrad")
 dest = pathlib.Path("teenygrad")
 
 for f in FILES:
   rd = open(src/f).read()
+  rd = rd.split("# --teenygrad--")[0]
   rd = rd.replace("from tinygrad.", "from teenygrad.")
   rd = rd.replace("import tinygrad.", "import teenygrad.")
+  rd = rd.replace("tinygrad.runtime.", "teenygrad.runtime.")
   with open(dest/f, "w") as f:
     f.write(rd)
