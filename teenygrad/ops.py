f1d090457725d5b95104d88eb0f1f27c5fff7f27
Pankaj Doharey <pankajdoharey@gmail.com>
2023-11-27 12:41:38 +0530

    Update ops.py
    Updates range bound.

--- teenygrad/ops.py
+++ teenygrad/ops.py
@@ -1,12 +1,12 @@
 from enum import Enum, auto
 from typing import Optional
 
-class UnaryOps(Enum): NOOP, EXP2, LOG2, CAST, SIN, SQRT, RECIP, NEG = range(1, 8) # noqa: E702
-class BinaryOps(Enum): ADD, SUB, MUL, DIV, MAX, MOD, CMPLT = range(1, 7) # noqa: E702
-class ReduceOps(Enum): SUM, MAX = range(1, 2) # noqa: E702
-class TernaryOps(Enum): MULACC, WHERE = range(1, 2) # noqa: E702
-class MovementOps(Enum): RESHAPE, PERMUTE, EXPAND, PAD, SHRINK, STRIDE = range(1, 6) # noqa: E702
-class LoadOps(Enum): EMPTY, RAND, CONST, FROM, CONTIGUOUS, CUSTOM = range(1, 6) # noqa: E702
+class UnaryOps(Enum): NOOP, EXP2, LOG2, CAST, SIN, SQRT, RECIP, NEG = range(1, 9) # noqa: E702
+class BinaryOps(Enum): ADD, SUB, MUL, DIV, MAX, MOD, CMPLT = range(1, 8) # noqa: E702
+class ReduceOps(Enum): SUM, MAX = range(1, 3) # noqa: E702
+class TernaryOps(Enum): MULACC, WHERE = range(1, 3) # noqa: E702
+class MovementOps(Enum): RESHAPE, PERMUTE, EXPAND, PAD, SHRINK, STRIDE = range(1, 7) # noqa: E702
+class LoadOps(Enum): EMPTY, RAND, CONST, FROM, CONTIGUOUS, CUSTOM = range(1, 7) # noqa: E702
 
 class Device:
   DEFAULT = "CPU"
